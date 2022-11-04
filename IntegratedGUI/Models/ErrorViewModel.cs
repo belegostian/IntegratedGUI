a726468c24b50d74c94b1f075d6331c1f612f535
belegostian <r10522701@ntu.edu.tw>
2022-11-04 12:36:41 +0800

    empty structure

---
+++ IntegratedGUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntegratedGUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
